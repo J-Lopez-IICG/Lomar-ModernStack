7db5185ab5213d0f8b631311fa7766bad44041be
Javier Lopez <javier.lopez.iicg@gmail.com>
2026-02-10 21:40:25 -0300

    feat: db primar

--- src/lomar_stack/pipelines/data_ingestion/pipeline.py
+++ src/lomar_stack/pipelines/data_ingestion/pipeline.py
@@ -1,14 +1,15 @@
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes.ingesta_nodes import reportar_produccion_actual
+from .nodes import ingestar_raw_data
+
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=reportar_produccion_actual,
-                inputs="produccion_actual",  # Nombre que pusimos en el catalog.yml
-                outputs="produccion_procesada",
-                name="nodo_reporte_aws",
+                func=ingestar_raw_data,  # La función genérica
+                inputs="sql_produccion_actual",  # El dataset específico del catálogo
+                outputs="raw_produccion_actual",  # Dónde se guardará
+                name="ingest_produccion_node",  # Nombre del paso en el proceso
             ),
         ]
     )
